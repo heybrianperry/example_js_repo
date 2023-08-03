c2d73a2a4b3a411e5d7a9116d2c92d0cc39470b2
Brian Perry <contact.brianperry@gmail.com>
2023-08-02 20:48:08 -0500

    Add vitest

---
+++ packages/api-client/tests/apiClient.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { ApiClient } from '../src/ApiClient';
+
+const baseUrl = 'https://dev-drupal-api-client-poc.pantheonsite.io';
+
+const client = new ApiClient(baseUrl);
+
+test('constructor sets baseUrl', () => {
+  expect(client.baseUrl).toBe(baseUrl);
+})
---
+++ packages/json-api-client/tests/JsonApiClient.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { JsonApiClient } from '../src/JsonApiClient';
+
+const baseUrl = 'https://dev-drupal-api-client-poc.pantheonsite.io';
+
+const client = new JsonApiClient(baseUrl);
+
+test('constructor sets baseUrl', () => {
+  expect(client.baseUrl).toBe(baseUrl);
+})
