c8628403abb6475aa751f62058913988e1743c67
heybrianperry <98431247+heybrianperry@users.noreply.github.com>
2025-08-19 17:48:16 -0500

    Update main.ts
    Disable example

--- examples/graphql-client/src/main.ts
+++ examples/graphql-client/src/main.ts
@@ -1,31 +1,31 @@
-import { GraphqlClient } from "@drupal-api-client/graphql-client";
+// import { GraphqlClient } from "@drupal-api-client/graphql-client";
 
-const baseUrl = import.meta.env.VITE_BASE_URL
-  ? import.meta.env.VITE_BASE_URL
-  : "https://drupal-api-demo.party";
+// const baseUrl = import.meta.env.VITE_BASE_URL
+//   ? import.meta.env.VITE_BASE_URL
+//   : "https://drupal-api-demo.party";
 
-async function main() {
-  const graphqlClient = new GraphqlClient(baseUrl, {
-    authentication: {
-      type: "Basic",
-      credentials: {
-        username: import.meta.env.VITE_USERNAME,
-        password: import.meta.env.VITE_PASSWORD,
-      },
-    },
-  });
-  const query = await graphqlClient.query(
-    `query GetArticles {
-      nodeArticles(first: 10) {
-        nodes {
-          title
-        }
-      }
-    }`,
-  );
+// async function main() {
+//   const graphqlClient = new GraphqlClient(baseUrl, {
+//     authentication: {
+//       type: "Basic",
+//       credentials: {
+//         username: import.meta.env.VITE_USERNAME,
+//         password: import.meta.env.VITE_PASSWORD,
+//       },
+//     },
+//   });
+//   const query = await graphqlClient.query(
+//     `query GetArticles {
+//       nodeArticles(first: 10) {
+//         nodes {
+//           title
+//         }
+//       }
+//     }`,
+//   );
 
-  document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <pre>${JSON.stringify(query, null, 2)}</pre>`;
-}
+//   document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <pre>${JSON.stringify(query, null, 2)}</pre>`;
+// }
 
-main();
+// main();
